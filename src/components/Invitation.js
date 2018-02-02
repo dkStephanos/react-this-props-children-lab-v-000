f6bc92e1b6e9de606af3ee96fe645037962ed7cc
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-02 16:52:45 -0500

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,13 @@
-// Code Invitation Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Invitation extends React.Component {
+  render() {
+    return (
+      <div className="invitation">
+        <h1 className="invitation-header">You've been invited!</h1>
+        <div className="invitation-body">{this.props.children}</div>
+      </div>
+    );
+  }
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,19 @@
 // Code ThemedDecoration Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ThemedDecoration extends React.Component {
+  render() {
+    const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+      return (
+        <div className={this.props.theme}>{child}</div> 
+      );
+    });
+ 
+    return (
+      <div className="themed-decoration">
+        {childrenWithWrapperDiv}        
+      </div>      
+    );
+  }
+}
